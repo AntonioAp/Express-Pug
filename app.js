dd95e75aa0719249b5aafec76a286c6652ec5510
AntonioAp <antonioaparicioh@gmail.com>
2021-05-31 14:05:06 +0200

    add pug files,jsfiles

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = require('./controllers/index')
+
+const app = express();
+const port = 3000;
+
+//motor de vistas
+app.set('view engine','pug');
+app.set('views','./views');
+
+
+//usar el router
+app.use('/',router);
