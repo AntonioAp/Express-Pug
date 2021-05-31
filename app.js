062368b84e562443b8e93617ff8710fa6054e00e
alejRes <alejrespal@gmail.com>
2021-05-31 17:20:13 +0200

    files .pug done, .css done

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const router = require('./controllers/index')
+const path = require('path')
 
 const app = express();
 const port = 3000;
+
+app.use("/public", express.static(path.join(__dirname, 'public')));
+
 
 //motor de vistas
 app.set('view engine','pug');
 app.set('views','./views');
 
-
 //usar el router
 app.use('/',router);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
